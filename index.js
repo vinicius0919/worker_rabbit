72d5349cb927e15f11977050ad1d72545a69a166
Vinícius Lima Ferreira <viniciuslima0919@gmail.com>
2025-12-22 21:51:12 -0300

    changing timeout value to 60 seconds

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const {
 
 async function processJob(rawText) {
   const controller = new AbortController();
-  const timeout = setTimeout(() => controller.abort(), 30000);
+  const timeout = setTimeout(() => controller.abort(), 60*1000);
 
   try {
     const res = await fetch(NLP_API_URL, {
